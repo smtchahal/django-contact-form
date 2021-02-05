76d526f9fb0f91486c276e2874aef3ef70506200
Sumit Chahal <smtchahal@gmail.com>
2021-02-06 00:56:13 +0530

    Add choices validation on subject

--- contact_form/models.py
+++ contact_form/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 
 class ContactFormSubmission(models.Model):
+    SUBJECT_CHOICES = [
+        ('Account',) * 2,
+        ('Payment',) * 2,
+        ('General feedback',) * 2,
+        ('Other',) * 2,
+    ]
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, blank=True, default='')
     email = models.EmailField()
-    subject = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200, choices=SUBJECT_CHOICES)
     message = models.CharField(max_length=3000)
